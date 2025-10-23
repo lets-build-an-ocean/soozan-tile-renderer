8aadc53f3b5797c229866846ba60b75578a124f6
Xtreme <sahebesabr11228@gmail.com>
2025-10-23 17:58:50 +0330

    set port for run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 # - Exposes the tile server on port 8080
 # - Uses the osm-data volume containing the imported database
 # - Runs in detached mode (background)
-docker run --name soozan-tile-renderer \
+docker run -d --name soozan-tile-renderer \
+  -p 8080:80 \
   -v ./iran-2025.osm.pbf:/data/region.osm.pbf \
   -v osm-data:/data/database/ \
   overv/openstreetmap-tile-server run
